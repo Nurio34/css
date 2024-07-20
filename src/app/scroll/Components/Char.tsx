f1b72ec2847bc62658d208388c71d4169866a7e3
Nurio34 <nurioonsoftware@gmail.com>
2024-07-21 00:03:27 +0300

    bugfix_vercel

--- src/app/scroll/Components/Char.tsx
+++ src/app/scroll/Components/Char.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function Char({
     ch,
@@ -11,19 +11,23 @@ function Char({
     scrollTop: number;
     length: number;
 }) {
-    const maxWidth = window.innerWidth;
+    const [randomX, setRandomX] = useState<number>(0);
+    const [randomY, setRandomY] = useState<number>(0);
 
-    const randomX = Math.floor(Math.random() * maxWidth);
-    const randomY = Math.floor(Math.random() * window.innerHeight);
+    useEffect(() => {
+        const maxWidth = window.innerWidth;
+        setRandomX(Math.floor(Math.random() * maxWidth));
+        setRandomY(Math.floor(Math.random() * window.innerHeight));
+    }, []);
 
     return (
         <span
             key={ind}
             className={`absolute transition-transform ${
                 scrollTop >= (ind + 1) * 100
-                    ? "text-primary top-0 left-0 capitalize font-bold "
+                    ? "text-primary top-0 left-0 capitalize font-bold"
                     : ""
-            } `}
+            }`}
             style={{
                 top: scrollTop >= (ind + 1) * 100 ? "50%" : randomY,
                 left:
@@ -32,14 +36,14 @@ function Char({
                     scrollTop >= (ind + 1) * 100
                         ? `translateY(-50%) translateX(calc(-50% - ${
                               (length - ind) * 1
-                          }rem)`
+                          }rem))`
                         : ""
                 }`,
                 filter: `${
                     scrollTop >= (ind + 1) * 100
                         ? "drop-shadow(0 0 10px white) drop-shadow(0 0 20px white)"
                         : ""
-                } `,
+                }`,
             }}
         >
             {ch}
